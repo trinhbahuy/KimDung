01c81dbb472644f511867ad6366282df3de68a75
trinhbahuy <trinh.ba.huy19@gmail.com>
2018-03-28 09:21:10 +0700

    create

---
+++ KimDung/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KimDung
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Click_Btn1(object sender, EventArgs e)
+        {
+            MessageBox.Show("loz");
+        }
+    }
+}
