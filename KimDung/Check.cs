9ae6adba4a05f13c84dcefaf725616995895be70
duchuy5700 <duchuy5700@gmail,com>
2018-04-03 22:09:06 +0700

    them class check

---
+++ KimDung/Check.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KiDung
+{
+    public class Check
+    {
+        public Check()
+        {
+        }
+
+        public bool checklength(string a)
+        {
+            if (a.Length > 7)
+                return false;
+            return true;
+        }
+    }
+}
+
